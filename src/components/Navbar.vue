f2d289801dd14b9f5de669505050a44feeca4d32
dyson <dysonthomas112@gmail.com>
2020-08-06 01:45:30 +0530

    commit

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -13,19 +13,19 @@
           </ul>
           <form class="form-inline my-2 my-lg-0">
             <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-            <b-button variant="success" v-b-modal.modal-1>Get Start</b-button>
-
-            <a class="btn btn-outline-success border-0 mx-2 my-2 my-sm-0" data-toggle="modal" data-target="#miniCart">
+            <b-button variant="success" v-if="status==0" v-b-modal.modal-1>Get Start</b-button>
+            
+            <router-link to="/" v-if="status==1" class="btn btn-outline-success border-0 mx-2 my-2 my-sm-0" data-toggle="modal" data-target="#miniCart">
               <i class="fas fa-cart-plus"></i>
-            </a>
-            <a class="btn btn-outline-success border-0  my-2 my-sm-0" data-toggle="modal" data-target="#miniCart">
+            </router-link>
+           <router-link to="/" v-if="status==1" class="btn btn-outline-success border-0  my-2 my-sm-0" data-toggle="modal" data-target="#miniCart">
               <i class="fa fa-users" aria-hidden="true"></i>
-            </a>
+            </router-link>
           </form>
         </div>
 
         <!-- Modal -->
-        <b-modal id="modal-1" title="Welcome To Vue Shop" hide-footer="true">
+        <b-modal id="modal-1" title="Welcome To Vue Shop" :hide-footer="true">
           <div>
             
             <b-tabs content-class="mt-3" align="center">
@@ -34,13 +34,14 @@
               
                   <form>
                     <div class="form-group">
-                      <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
+                      <input type="email" v-model="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
                         placeholder="Enter email"  required>
                     </div>
                     <div class="form-group">
-                      <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" required>
+                      <input type="password" v-model="password" class="form-control" id="exampleInputPassword1" placeholder="Password" required>
                     </div>
-                    <button type="submit" class="btn btn-primary mr-2">Sign In</button>
+                    <p>Forgot Pasword?<span   @click="forgotpassword" class="frg"> Click Here</span></p>
+                    <button type="submit" @click="login" class="btn btn-primary mr-2">Sign In</button>
                     <button type="submit" @click="close" class="btn btn-primary">Close</button>
                   </form>
                 
@@ -48,22 +49,22 @@
               <b-tab title="Sign Up">
                   <form>
                          <div class="form-group">
-                    <input type="text" class="form-control" placeholder="Enter Your Name" required>  
+                    <input type="text" v-model="name" class="form-control" placeholder="Enter Your Name" required>  
                          </div>
                     <div class="form-group"> 
                       
-                      <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
+                      <input type="email" v-model="email1" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
                         placeholder="Enter email" required>
                     </div>
                     <div class="form-group">
-                      <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" required>
+                      <input type="password" v-model="password1" class="form-control" id="exampleInputPassword1" placeholder="Password" required>
                     </div>
                     
                     
-                    <button type="submit" class="btn btn-primary mr-2">Sign Up</button>
+                    <button type="submit" @click="register" class="btn btn-primary mr-2">Sign Up</button>
                     <button type="submit" @click="close"  class="btn btn-primary">Close</button>
                   </form>
-                </p>
+             
               </b-tab>
             </b-tabs>
           </div>
@@ -77,14 +78,99 @@
 </template>
 
 <script>
+import {fb,db} from '../firebase'
+
+import firebase from '../firebase'
   export default {
     name: "Navbar",
     props: {
       msg: String
     },
+      data(){
+      return {
+          name:"",
+          email:"",
+          password:"",
+          email1:"",
+          password1:"",
+          status:"0"
+      }
+  },
+   created(){
+     fb.auth().onAuthStateChanged((user)=>{
+     if (user) {
+       console.log(user)
+      this.status=1
+     } else {
+    
+  }
+});
+
+
+  },
     methods:{
       close(){
         this.$bvModal.hide('modal-1')
+      },
+      
+      forgotpassword(){
+        var auth = fb.auth();
+        console.log(auth)
+        // var emailAddress = this.email;
+        auth.sendPasswordResetEmail(this.email).then(function() {
+        alert("There is a reset link in your email address");
+        }).catch(function(error) {
+         console.log(error)
+        })
+      },
+        login(){
+
+          fb.auth().signInWithEmailAndPassword(this.email, this.password)
+                        .then(() => {
+                           this.$bvModal.hide('modal-1')
+                          alert("sucess")
+                        })
+                        .catch(function(error) {
+                            // Handle Errors here.
+                            var errorCode = error.code;
+                            var errorMessage = error.message;
+                            if (errorCode === 'auth/wrong-password') {
+                                alert('Wrong password.');
+                            } else {
+                                alert(errorMessage);
+                            }
+                            console.log(error);
+                    });
+
+      },
+      register(){
+            fb.auth().createUserWithEmailAndPassword(this.email1, this.password1)
+                .then((user) => {
+                  alert("Sucess")
+                      this.$bvModal.hide('modal-1')
+                    db.collection("profiles").doc(user.user.uid).set({
+                        name: this.name
+                    })
+                    .then(function() {
+                        console.log("Document successfully written!");
+                    })
+                    .catch(function(error) {
+                        console.error("Error writing document: ", error);
+                    });
+
+                    // this.$router.replace('admin');
+                })
+                .catch(function(error) {
+                // Handle Errors here.
+                var errorCode = error.code;
+                var errorMessage = error.message;
+                if (errorCode == 'auth/weak-password') {
+                    alert('The password is too weak.');
+                } else {
+                    alert(errorMessage);
+                }
+                console.log(error);
+            });
       }
     }
   };
@@ -104,6 +190,10 @@
     }
     .modal-img{
       width: 20%;
+    }
+    .frg{
+      color: blue;
+      cursor: pointer;
     }
 
   }
